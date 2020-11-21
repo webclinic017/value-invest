aede50a4e52ddaaf4fbad70be3f4f5131712dd79
Haley Morgan <haleymorgan3264@gmail.com>
2020-11-20 22:31:03 -0500

    first test of API

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-# Main python script
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+import os
+from dotenv import load_dotenv
 
+# Load env vars from .env
+load_dotenv()
+token = os.environ.get("av-api-token")
+
+ts = TimeSeries(key=token, output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='LOW',interval='1min', outputsize='full')
+pprint(data.head(2))
