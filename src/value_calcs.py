e97aac3a943585a3b3da34e5967c50ebd497b4cf
Haley Morgan <haleymorgan3264@gmail.com>
2020-11-22 00:27:41 -0500

    perform per share calculations and make simple plot

--- src/value_calcs.py
+++ src/value_calcs.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
-stock = 'LOW'
+stock = 'ABBV'
 
 # Get CSVs for financial statements
 dir = os.getcwd()
@@ -17,9 +18,7 @@ stock_income_stmts = income_stmt_all[income_stmt_all.Ticker == stock]
 
 # Create ultimate stock data df
 stock_income_data = stock_income_stmts[["Publish Date", "Report Date", "Shares (Basic)", "Revenue", "Net Income"]].copy()
-
-# Divide the nums by share nums to get per share values -- we will do this at the end with everything
-stock_income_data[["Sales Per Share", "EPS"]] = stock_income_data[["Revenue", "Net Income"]].div(stock_income_data["Shares (Basic)"], axis=0)
+print(stock_income_data)
 
 # ======== 2. Next process the balance sheets 
 balance_sheets_all = pd.read_csv(balance_stmt_file, sep=";")
@@ -27,7 +26,8 @@ balance_sheets_all = pd.read_csv(balance_stmt_file, sep=";")
 stock_balance_sheets = balance_sheets_all[balance_sheets_all.Ticker == stock]
 
 # Get cols from balance sheet we are interested in
-stock_balance_sheets = stock_balance_sheets[["Publish Date", "Report Date", "Total Equity"]].copy()
+stock_balance_sheets = stock_balance_sheets[["Total Equity"]].copy()
+
 
 # ======== 3. Next process the cash flow stmts 
 cash_flow_stmts = pd.read_csv(cash_flow_stmt_file, sep=";")
@@ -35,6 +35,17 @@ cash_flow_stmts = pd.read_csv(cash_flow_stmt_file, sep=";")
 stock_cashflow_stmts = cash_flow_stmts[cash_flow_stmts.Ticker == stock]
 
 # Get cols from cash flow statement we are interested in 
-stock_cashflow_stmts = stock_cashflow_stmts[["Publish Date", "Report Date", "Net Cash from Operating Activities"]].copy()
+stock_cashflow_stmts = stock_cashflow_stmts[["Net Cash from Operating Activities"]].copy()
 
+
+# Join all 3 dfs
+stock_data = pd.concat([stock_income_data, stock_balance_sheets, stock_cashflow_stmts], axis=1, sort=False)
+
+# Divide the nums by # shares to get per share values
+stock_data[["Sales Per Share", "EPS", "Equity Per Share", "Op. Cash Per Share"]] = stock_data[["Revenue", "Net Income", "Total Equity", "Net Cash from Operating Activities"]].div(stock_data["Shares (Basic)"], axis=0)
+
+print(stock_data)
+
+stock_data.plot(x="Report Date", y=["EPS", "Sales Per Share", "Equity Per Share", "Op. Cash Per Share"])
+plt.show()
 
